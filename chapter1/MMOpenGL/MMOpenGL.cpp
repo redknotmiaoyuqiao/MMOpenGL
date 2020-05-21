c7cdc3915da6fb6ed30a3e875e29df0d43ebe9e6
yinhuiyao <632212519@qq.com>
2020-05-22 00:49:26 +0800

    chapter1

---
+++ chapter1/MMOpenGL/MMOpenGL.cpp
@@ -0,0 +1,13 @@
+﻿#include <stdio.h>
+
+#include <glad/glad.h>
+#include <GLFW/glfw3.h>
+
+int main()
+{
+	glfwInit();
+	gladLoadGLLoader((GLADloadproc)glfwGetProcAddress);
+
+	printf("Hello MMOpenGL\n");
+	return 0;
+}
